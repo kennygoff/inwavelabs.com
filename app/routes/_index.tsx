3e115e50917eaf41cf546ba53ec8e4ada4a47259
Kenny Goff <goff.kenneth@gmail.com>
2023-07-18 18:57:35 -0400

    Cleanup homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,44 +9,82 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="text-sm px-6 py-12 leading-6 font-mono h-screen overflow-y-scroll md:text-lg md:leading-9 md:px-8 md:py-12 mx-auto max-w-5xl">
+    <div className="text-md px-6 py-12 leading-7 font-mono h-screen overflow-y-scroll md:text-lg md:leading-9 md:px-8 md:py-12 mx-auto max-w-5xl">
       <main>
         <section className="mb-12">
-          <h1 className="mb-4 md:mb-0">
+          <h1 className="mb-12 md:mb-6">
             <strong className="font-black">Inwave Labs</strong> is an
             independent software company.
           </h1>
-          <p className="mb-4 md:mb-0">
-            It is run by{" "}
+        </section>
+        <section className="mb-12">
+          <p className="mb-3 md:mb-0">
+            We craft software of all kinds — apps, games, tools and websites.
+          </p>
+        </section>
+        <section className="mb-12">
+          <p className="mb-3 md:mb-0">
+            Inwave Labs is run by{" "}
             <a href="https://kennygoff.com" className="font-black underline">
               Kenny Goff
             </a>
-            , a developer and designer.
+            , a creative developer.
           </p>
-          <p className="mb-4 md:mb-0">
-            We craft software of all kinds — apps, games, tools and websites.
-          </p>
-          <p className="mb-4 md:mb-0">
-            We made the game <strong className="font-black">Double Turn</strong>
+          <p className="mb-3 md:mb-0">
+            We made the game{" "}
+            <a
+              href="https://doubleturngame.com"
+              className="font-black underline"
+            >
+              Double Turn
+            </a>
             , a pro-wrestling platform fighter.
           </p>
           <p className="mb-4 md:mb-0">We are working on something new!</p>
         </section>
         <section className="mb-12">
           <h2 className="font-black">Working principles</h2>
-          <ol className="list-decimal list-inside">
-            <li>Build a sustainable business that will last</li>
-            <li>Care and dignity for people who work for or with us</li>
-            <li>Fight for independence and stay small</li>
-            <li>Trust must be perpetually earned</li>
-            <li>Unwelcome to the intolerant</li>
-            <li>Share in our victories</li>
-          </ol>
+          <div
+            role="list"
+            className="grid grid-cols-[20px_auto] md:grid-cols-[24px_auto] gap-x-2"
+          >
+            <div className="select-none" aria-hidden>
+              1.
+            </div>
+            <div role="listitem">
+              Build a sustainable business that will last
+            </div>
+            <div className="select-none" aria-hidden>
+              2.
+            </div>
+            <div role="listitem">
+              Care and dignity for people who work with us
+            </div>
+            <div className="select-none" aria-hidden>
+              3.
+            </div>
+            <div role="listitem">Fight for independence and stay small</div>
+            <div className="select-none" aria-hidden>
+              4.
+            </div>
+            <div role="listitem">Trust must be perpetually earned</div>
+            <div className="select-none" aria-hidden>
+              5.
+            </div>
+            <div role="listitem">Unwelcome to the intolerant</div>
+            <div className="select-none" aria-hidden>
+              6.
+            </div>
+            <div role="listitem">Share in our victories</div>
+          </div>
         </section>
         <section className="mb-12">
           <h2 className="font-black">Get in touch</h2>
           <p>
-            <span aria-hidden className="select-none">
+            <span
+              aria-hidden
+              className="select-none inline-block w-[20px] md:w-[24px] mr-2"
+            >
               &rarr;{" "}
             </span>
             kenny@inwavelabs.com
@@ -55,7 +93,7 @@ export default function Index() {
       </main>
       <footer>
         <p>Crafted in Hoboken, NJ</p>
-        Made with <span style={{ fontFamily: "none" }}>❤</span> by{" "}
+        Made with <span style={{ fontFamily: "none" }}>♥︎</span> by{" "}
         <a href="https://kennygoff.com" className="font-black underline">
           Kenny Goff
         </a>
